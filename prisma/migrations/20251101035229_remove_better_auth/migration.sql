38320d9f89855e1d53fb960c78444ca35f4b0e56
raworiginal <118763950+raworiginal@users.noreply.github.com>
2025-10-31 21:00:06 -0700

    removed better auth

---
+++ prisma/migrations/20251101035229_remove_better_auth/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the `account` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `verification` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."account" DROP CONSTRAINT "account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."session" DROP CONSTRAINT "session_userId_fkey";
+
+-- DropTable
+DROP TABLE "public"."account";
+
+-- DropTable
+DROP TABLE "public"."session";
+
+-- DropTable
+DROP TABLE "public"."user";
+
+-- DropTable
+DROP TABLE "public"."verification";
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import logger from "morgan";
-import { toNodeHandler } from "better-auth/node";
-import { auth } from "./lib/auth";
 
 dotenv.config();
 
@@ -12,8 +10,6 @@ const port = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(logger("dev"));
-
-app.all("/api/auth/{*any}", toNodeHandler(auth));
 app.use(express.json());
 
 app.get("/", (_, res: Response) => {
